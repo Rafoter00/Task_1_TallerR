957754c2c95d70ba4dd8c3357339a51354f69043
Rafotero00 <rd.otero@uniandes.edu.co>
2021-09-29 19:43:58 -0500

    Desarrollo pregunta 2

--- Taller 1 Script.R
+++ Taller 1 Script.R
@@ -22,8 +22,20 @@ if(!require(pacman)) install.packages("pacman") ; require(pacman) # Instalar la 
 p_load(rio,skimr,tidyverse,readxl,haven,WriteXLS) # Llamar y/o instalar las librerías de la clase
 Sys.setlocale("LC_CTYPE", "en_US.UTF-8") # Encoding UTF-8
 
-
+#Limpieza de datos
 data_cultivos = import(file= "task_r_202102/task_1/data/input/cultivos.xlsx", col_names = TRUE, skip = 8) #Traer Base cultivos.xlsx
-data_cultivos2<-data_cultivos[-c(352, 348, 341,333,302,291,272,236,221,216,212,207,200,195,188,160,151,122,105,99,86,69,63,9,353,354,355),]#Elimino Filas de Totales
-data_cultivos2 %>%  select(starts_with(c("CODDEPTO","CODMPIO"))) %>% head()
-data_cultivos_limpia = subset(data_cultivos2, select = -c(CODDEPTO,CODMPIO))#Limpio codigos de departamento y municipio
+data_cultivos=data_cultivos[-c(352, 348, 341,333,302,291,272,236,221,216,212,207,200,195,188,160,151,122,105,99,86,69,63,9,353,354,355),]#Elimino Filas de Totales
+data_cultivos = subset(data_cultivos, select = -c(CODDEPTO,CODMPIO))#Limpio codigos de departamento y municipio ( en mi opinion creo que los codigos son redundantes si ya tenemos nombres)
+years = 1999:2019 %>% as.character() # Determino un vector de años
+for(var in years){
+data_cultivos[,var] = as.numeric(data_cultivos[,var])
+ }# convierto las palabras a numericas
+data_cultivos = data_cultivos %>% rename_with(tolower) #Pongo todos los nombres de columnas en minuscula
+data_cultivos=data_cultivos %>% mutate(departamento = tolower(departamento))#Pongo en minusculas informacion de columna "departamento"
+data_cultivos=data_cultivos %>% mutate(municipio = tolower(municipio))#Pongo en minuscula informacion de columna "municipio"
+
+#Pivot a formato long
+Pivotlong_datacultivos=data_cultivos %>% pivot_longer(!departamento:municipio,names_to="año",values_to="numero de hectareas")#Pivoteo unicamente los años dejando estatico el departamento y el municipio
+
+
+
