95cb8ef0eea60d0c0f62a500c787e8ef184f87a3
Rafotero00 <rd.otero@uniandes.edu.co>
2021-10-03 21:38:44 -0500

    Task 1 Final terminado

--- Taller 1 Script.R
+++ Taller 1 Script.R
@@ -1,6 +1,10 @@
 #Task 1 Taller R
 #Rafael Otero 201821640
+#Maria Paula Alvarez 201820569
+#Version de R: R version 4.1.0 (2021-05-18) ,Platform: x86_64-apple-darwin17.0 (64-bit), Running under: macOS Big Sur 11.5.2
 
+
+#Taller 1
 rm(list = ls())
 rm(list = ls()) # limpia el entorno de R
 if(!require(pacman)) install.packages(pacman)
@@ -17,6 +21,11 @@ is.numeric(vector_impares)
 
 vector_pares=vector1_100[-vector_impares] #Vector Pares
 
+#Otra forma de hacerlo
+vector=c(1:100)#Vector de 1 a 100
+
+v_pares=vector[!vector %in% vector_impares]#Vector que contiene los datos "vector" que no están en el vector de numeros impares
+
 #Pregunta 2:
 rm(list = ls()) # limpia el entorno de R
 if(!require(pacman)) install.packages("pacman") ; require(pacman) # Instalar la librería pacman
@@ -25,18 +34,27 @@ Sys.setlocale("LC_CTYPE", "en_US.UTF-8") # Encoding UTF-8
 
 #Limpieza de datos
 data_cultivos = import(file= "task_r_202102/task_1/data/input/cultivos.xlsx", col_names = TRUE, skip = 8) #Traer Base cultivos.xlsx
-data_cultivos=data_cultivos[-c(352, 348, 341,333,302,291,272,236,221,216,212,207,200,195,188,160,151,122,105,99,86,69,63,9,353,354,355),]#Elimino Filas de Totales
-data_cultivos = subset(data_cultivos, select = -c(CODDEPTO,CODMPIO))#Limpio codigos de departamento y municipio ( en mi opinion creo que los codigos son redundantes si ya tenemos nombres)
+
+is.na(data_cultivos$MUNICIPIO) %>% table() #Revisar cuantas observaciones tienen NA
+cultivos = data_cultivos %>% drop_na(MUNICIPIO) #Eliminar las observaciones que tengan NA en MUNICIPIO, estas corresponden a los totales por departamento
+is.na(cultivos$MUNICIPIO) %>% table() #Verificar que no hayan quedado observaciones con NA en MUNICIPIO
+cultivos = cultivos[-c(329),] #Eliminar la fila de referencias
+
+cultivos = subset(cultivos, select = -c(CODDEPTO,CODMPIO))#Limpio codigos de departamento y municipio ( en mi opinion creo que los codigos son redundantes si ya tenemos nombres)
+
+
 years = 1999:2019 %>% as.character() # Determino un vector de años
 for(var in years){
-data_cultivos[,var] = as.numeric(data_cultivos[,var])
- }# convierto las palabras a numericas
-data_cultivos = data_cultivos %>% rename_with(tolower) #Pongo todos los nombres de columnas en minuscula
-data_cultivos=data_cultivos %>% mutate(departamento = tolower(departamento))#Pongo en minusculas informacion de columna "departamento"
-data_cultivos=data_cultivos %>% mutate(municipio = tolower(municipio))#Pongo en minuscula informacion de columna "municipio"
+  cultivos[,var] = as.numeric(cultivos[,var])
+}# convierto las palabras a numericas
+
+cultivos = cultivos %>% rename_with(tolower) #Pongo todos los nombres de columnas en minuscula
+cultivos=cultivos %>% mutate(departamento = tolower(departamento))#Pongo en minusculas informacion de columna "departamento"
+cultivos=cultivos %>% mutate(municipio = tolower(municipio))#Pongo en minuscula informacion de columna "municipio"
 
 #Pivot a formato long
-Pivotlong_datacultivos=data_cultivos %>% pivot_longer(!departamento:municipio,names_to="año",values_to="numero de hectareas")#Pivoteo unicamente los años dejando estatico el departamento y el municipio
+
+Pivotlong_cultivos=cultivos %>% pivot_longer(!departamento:municipio,names_to="año",values_to="numero de hectareas")#Pivoteo unicamente los años dejando estatico el departamento y el municipio
 
 #Pregunta 3
 rm(list = ls()) # limpia el entorno de R
@@ -62,20 +80,27 @@ geih = left_join(cabecera_caracteristcas,cabecera_desocupados,c("secuencia_p","o
 Total_ocupados_desocupado_edad= geih %>% group_by(P6040) %>% summarise(ocupado=table(ocupado),desocupado=table(desocupado))#Total de desocupados y ocupados por edad
 Total_ocupados_desocupado_genero= geih %>% group_by(P6020) %>% summarise(ocupado=table(ocupado),desocupado=table(desocupado))#Total de desocupados y ocupados por genero
 Total_ocupados_desocupado_ESC= geih %>% group_by(P6020,ESC) %>% summarise(ocupado=table(ocupado),desocupado=table(desocupado))#Total de desocupados y ocupados por genero y años de escolaridad
-Ingreso_promedio_mediano_genero_edad= geih %>%  group_by(P6020,P6040)  %>% summarise(ingresos_promedio = mean(INGLABO, na.rm = TRUE),ingreso_mediano = median(INGLABO,na.rm= TRUE)) %>% drop_na(ingresos_promedio) %>% subset(ingresos_promedio>0) #Ingreso Promedio y mediano por sexo y edad
-Ingreso_promedio_mediano_genero_escolaridad= geih %>%  group_by(P6020,ESC)  %>% summarise(ingresos_promedio = mean(INGLABO, na.rm = TRUE),ingreso_mediano = median(INGLABO,na.rm= TRUE))%>% drop_na(ingresos_promedio) %>% subset(ingresos_promedio>0) #Ingreso Promedio y mediano por sexo y años de escolaridad
+
+Ingreso_promedio_mediano_genero_edad= geih %>%  group_by(P6020,P6040)  %>% summarise(ingresos_promedio = mean(INGLABO, na.rm = TRUE),ingreso_mediano = median(INGLABO,na.rm= TRUE), varianza_ingreso=var(INGLABO,na.rm=TRUE)) %>% drop_na(ingresos_promedio) %>% subset(ingresos_promedio>0) #Ingreso Promedio y mediano por sexo y edad
+Ingreso_promedio_mediano_genero_escolaridad= geih %>%  group_by(P6020,ESC)  %>% summarise(ingresos_promedio = mean(INGLABO, na.rm = TRUE),ingreso_mediano = median(INGLABO,na.rm= TRUE), varianza_ingreso=var(INGLABO,na.rm=TRUE)) %>% drop_na(ingresos_promedio) %>% subset(ingresos_promedio>0) #Ingreso Promedio y mediano por sexo y años de escolaridad
+
 
 if(!require(pacman)) install.packages("pacman") ; require(pacman) # Instalar la librería pacman
 p_load(tidyverse , rio , skimr , RColorBrewer , ggthemes , hrbrthemes , igraph) 
 
+
 graph1 = ggplot(Total_ocupados_desocupado_edad) + geom_point(aes(x=P6040,y=ocupado,col="red")) #Grafica de dispersion edad vs ocupados
+graph1
 graph2 = ggplot(Total_ocupados_desocupado_edad) + geom_point(aes(x=P6040,y=desocupado)) #Grafica de dispersion edad vs desocupados
+graph2
 genero<-cut(Ingreso_promedio_mediano_genero_edad$P6020,breaks=c(0,1,2),labels=c("mujer","hombre"))
 Ingreso_promedio_mediano_genero_edad$P6020=genero#Vuelvo variable P6020 categorica
 Genero<-cut(Ingreso_promedio_mediano_genero_escolaridad$P6020,breaks=c(0,1,2),labels=c("mujer","hombre"))
 Ingreso_promedio_mediano_genero_escolaridad$P6020=Genero #Vuelvo variable P6020 categorica
 graph3 = ggplot(Ingreso_promedio_mediano_genero_edad)+geom_point(aes(x=P6040,y=ingresos_promedio,color=genero)) #Grafica de dispersion edad vs Ingreso promedio discriminado por genero
+graph3
 graph4 = ggplot(Ingreso_promedio_mediano_genero_escolaridad)+geom_point(aes(x=ESC,y=ingresos_promedio,color=Genero))+ theme_pander() + scale_colour_pander()#Grafica de dispersion años escolarid vs ingreso promedio discriminado por genero
+graph4
 graph5 = ggplot(data = Ingreso_promedio_mediano_genero_escolaridad , aes(x = ESC , y =ingreso_mediano , fill = Genero)) + 
   geom_bar(stat = "identity", position = "dodge") +  
   geom_text(aes(label = ESC ),  
@@ -89,6 +114,7 @@ graph5 = ggplot(data = Ingreso_promedio_mediano_genero_escolaridad , aes(x = ESC
        y = "Ingreso Mediano", 
        fill = "Genero") + theme_bw()
 #El ultimo comando es una graficos de barras que muestra el ingreso mediano segun los años de escolaridad discriminado por genero
+graph5
 
 #Guardar
 ggsave(plot=graph1, file = "views/ejemplo1.jpeg")
@@ -96,4 +122,3 @@ ggsave(plot=graph2, file = "views/ejemplo2.jpeg")
 ggsave(plot=graph3, file = "views/ejemplo3.jpeg")
 ggsave(plot=graph4, file = "views/ejemplo4.jpeg")
 ggsave(plot=graph5, file = "views/ejemplo5.jpeg")
-                                                                                                                               
